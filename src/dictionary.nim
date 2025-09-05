cd5fc74c58698d759c2a5a509de6e59fe53b4949
!!Dean <dwhall256@gmail.com>
2025-09-05 00:02:05 -0500

    Fix node argument naming
    Some procs use the DictNode arg as a node in the tree.
    Some procs use the DictNode arg as the root of the dictionary.
    Use "node" for the former and "dict" for the latter.

--- src/dictionary.nim
+++ src/dictionary.nim
@@ -20,12 +20,12 @@ proc `[]`(node: DictNode, c: char): DictNode {.inline.} =
     return nil
   node.letters[c.toLowercaseLetter]
 
-proc addWord(node: DictNode, word: string) =
+proc addWord(dict: DictNode, word: string) =
   ## Adds a word to the dictionary.
   let lowerWord = word.toLower
   if not all(lowerWord, proc(c:char): bool = c in LowercaseLetters):
     raise newException(ValueError, "Word contains non-letter characters.")
-  var n = node
+  var n = dict
   for c in lowerWord:
     if c notin n.letters:
       let newNode = DictNode()
@@ -33,21 +33,21 @@ proc addWord(node: DictNode, word: string) =
     n = n.letters[c]
   n.isWord = true
 
-proc addWordsFromFile*(root: DictNode, fn: string): int =
+proc addWordsFromFile*(dict: DictNode, fn: string): int =
   ## Adds words from a file to the dictionary.
   ## Returns the number of words added.
   # Expects the file to have one word per line.
   for line in lines(fn):
     let word = line.strip()
-    root.addWord(word)
+    dict.addWord(word)
     inc result
 
-proc contains*(root: DictNode, word: string): bool =
+proc contains*(dict: DictNode, word: string): bool =
   ## Returns true if the word exists in dict.
   let lowerWord = word.toLower
   if not all(lowerWord, proc(c:char): bool = c in LowercaseLetters):
     return false
-  var n = root
+  var n = dict
   for c in lowerWord:
     if n[c] == nil:
       return false
