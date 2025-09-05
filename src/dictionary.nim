83bd46a0a41fc9096aa801a0dcf15f657be733e8
!!Dean <dwhall256@gmail.com>
2025-09-04 23:39:32 -0500

    Misc non-functional
    Rearrange procs in read-down order.
    Remove unused code.
    Add comments

--- src/dictionary.nim
+++ src/dictionary.nim
@@ -13,24 +13,15 @@ converter toLowercaseLetter*(c: char): LowercaseLetter =
   LowercaseLetter(c)
 
 proc `[]`(node: DictNode, c: char): DictNode {.inline.} =
+  ## Returns the child node for the given letter, or nil if it doesn't exist.
   if c notin LowercaseLetters:
     return nil
   if c.toLowercaseLetter notin node.letters:
     return nil
   node.letters[c.toLowercaseLetter]
 
-proc `$`(node: DictNode): string =
-  if node == nil:
-    return "nil"
-  result = "{"
-  for c in 'a'..'z':
-    if node[c] != nil:
-      if result.len > 1:
-        result.add(" ")
-      result.add(c)
-  result.add("}")
-
 proc addWord(node: DictNode, word: string) =
+  ## Adds a word to the dictionary.
   let lowerWord = word.toLower
   if not all(lowerWord, proc(c:char): bool = c in LowercaseLetters):
     raise newException(ValueError, "Word contains non-letter characters.")
@@ -43,12 +34,16 @@ proc addWord(node: DictNode, word: string) =
   n.isWord = true
 
 proc addWordsFromFile*(root: DictNode, fn: string): int =
+  ## Adds words from a file to the dictionary.
+  ## Returns the number of words added.
+  # Expects the file to have one word per line.
   for line in lines(fn):
     let word = line.strip()
     root.addWord(word)
     inc result
 
 proc contains*(root: DictNode, word: string): bool =
+  ## Returns true if the word exists in dict.
   let lowerWord = word.toLower
   if not all(lowerWord, proc(c:char): bool = c in LowercaseLetters):
     return false
@@ -59,10 +54,4 @@ proc contains*(root: DictNode, word: string): bool =
     n = n[c]
   return n.isWord
 
-#proc marshal*(node: DictNode): string =
-#  $$node
 
-# TODO: store the dictionary to a file via:
-# * serialize the dict to a string
-# * write the string to a file
-# and the reverse
--- src/main.nim
+++ src/main.nim
@@ -2,22 +2,19 @@ import std/[os, paths, strformat]
 import cmd
 import dictionary
 
-proc addWordsFromBuiltinFiles(root: DictNode) =
-  echo "Loading built-in word files..."
-  let cwd = paths.getCurrentDir().string
-  let builtinWordFiles = ["4160offi.cia", "113809of.fic"]
-  for fn in builtinWordFiles:
-    let fullPath = cwd / "resources" / fn
-    let count = root.addWordsFromFile(fullPath)
-    echo &"{count} words added from {fn}"
-  echo "Ready."
+proc addWordsFromBuiltinFiles(root: DictNode)
 
 proc main =
   var root = DictNode()
   root.addWordsFromBuiltinFiles()
 
+  # a closure so that 'root' is captured
   proc loadCommand(ctx: var CmdPrompt, input: seq[string]) =
-    if input.len != 1: return
+    ## Expects exactly one argument, the filename, in input.
+    ## Loads the words from the file into the dictionary, root.
+    if input.len != 1:
+      echo "Expected exactly one argument, <filename>"
+      return
     let fn = input[0]
     if not fileExists(fn):
       echo &"File \"{fn}\" does not exist"
@@ -30,6 +27,16 @@ proc main =
   let loadCmd = Command(name: "load", desc: "Loads a word file into the dictionary", exeCmd: loadCommand)
   var prompt = newCmdPrompt(promptString="dict> ", commands=[loadCmd,])
   prompt.run()
+
+proc addWordsFromBuiltinFiles(root: DictNode) =
+  echo "Loading built-in word files..."
+  let cwd = paths.getCurrentDir().string
+  let builtinWordFiles = ["4160offi.cia", "113809of.fic"]
+  for fn in builtinWordFiles:
+    let fullPath = cwd / "resources" / fn
+    let count = root.addWordsFromFile(fullPath)
+    echo &"{count} words added from {fn}"
+  echo "Ready."
 
 when isMainModule:
   main()
